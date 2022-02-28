2fc3e8609fccd8edc181143a597b4501bf5e5e78
percedersved <per.cedersved@gmail.com>
2022-02-28 14:19:33 +0100

    Made format changes to !hjälp

--- src/main/java/se/skaegg/discordbot/handlers/Help.java
+++ src/main/java/se/skaegg/discordbot/handlers/Help.java
@@ -9,17 +9,17 @@ public class Help implements EventHandler {
     public Mono<Void> process(Message eventMessage) {
 
         String helpText = "" +
-                "!ping - Kolla om jag lever\n" +
-                "!bolagetöppet - Öppettider för Bolaget i Norrtälje\n" +
-                "!codenames - Skapar en länk för Codenames på horsepaste.com\n" +
-                "!lag [Namn kommaseparerat] - Slumpar fram 2 lag utifrån namnen som angivits\n" +
-                "!lagvoice - Tar alla namn som är i någon voicekanal och slumpar 2 lag\n" +
-                "!fredag - Är det fredag?\n" +
-                "!film [Film- eller seriennamn på orginalspråk] - Visa information om film/serie från OMDB api" +
-                "!nedräkning [Namn på nedräkning] - Visar hur lång tid det är kvar till datumet på nedräkningen" +
-                "!nynedräkning [Namn på nedräkning, datum i format yyyy-MM-dd HH:mm] - Lägger till nedräkning" +
-                "!nedräkningar - Listar nedräkningar med namn och ID" +
-                "!tabortnedräkning [ID]- Tar bort nedräkning";
+                "`!ping` - Kolla om jag lever\n" +
+                "`!bolagetöppet` - Öppettider för Bolaget i Norrtälje\n" +
+                "`!codenames` - Skapar en länk för Codenames på horsepaste.com\n" +
+                "`!lag [Namn kommaseparerat]` - Slumpar fram 2 lag utifrån namnen som angivits\n" +
+                "`!lagvoice` - Tar alla namn som är i någon voicekanal och slumpar 2 lag\n" +
+                "`!fredag` - Är det fredag?\n" +
+                "`!film [Film- eller seriennamn på orginalspråk]` - Visa information om film/serie från OMDB api\n" +
+                "`!nedräkning [Namn på nedräkning]` - Visar hur lång tid det är kvar till datumet på nedräkningen\n" +
+                "`!nynedräkning [Namn på nedräkning, datum i format yyyy-MM-dd HH:mm]` - Lägger till nedräkning\n" +
+                "`!nedräkningar` - Listar nedräkningar med namn och ID\n" +
+                "`!tabortnedräkning [ID]` - Tar bort nedräkning\n";
 
         return Mono.just(eventMessage)
                 .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
